248a190a5403ac95d4e9c8ceefff075d5b94d537
Nathan Wright <nathanw128@gmail.com>
2018-03-14 10:12:07 +0000

    documentation

--- src/API/Letters.php
+++ src/API/Letters.php
@@ -12,14 +12,27 @@ use Stannp\StannpPhp as StannpPhp;
 
 class Letters extends StannpPhp
 {
+    /**
+     * Creates a letter
+     * 
+     * @param string $recipient  Recipient ID
+     * @param string $background Background URL
+     * @param string $template   Template ID
+     * @param string $pages      String containing basic HTML. Pages separated by sending an array
+     * @param string $file       PDF / DOC file directly. This value can be either a binary file or a URL to a file
+     * @param bool   $test       Produce a sample PDF and not dispatch the letter
+     * @param bool   $duplex     Print both side of the letter
+     * 
+     * @return JSON An encoded JSON object
+     */
     public function createLetter(
-        $test,
         $recipient,
         $background,
         $template,
         $pages,
         $file,
-        $duplex
+        $test = true,
+        $duplex = false
     ) {
         $path = "/letters/create";  
         $params = array(
@@ -35,10 +48,20 @@ class Letters extends StannpPhp
         return $this->post($path, $params);
     }
 
+
+    /**
+     * Posts a letter based on file input
+     * 
+     * @param string $pdf     URL or binary file of the pdf file to print and post.
+     * @param string $country ISO alpha 2 country code. For example “GB”, “US”, “FR” or “DE”.
+     * @param bool   $test    Produce a sample PDF and not dispatch the letter
+     * 
+     * @return JSON An encoded JSON object
+     */
     public function postLetter(
-        $test,
-        $country,
-        $pdf
+        $pdf,
+        $country = "GB",
+        $test = true
     ) {
         $path = "/letters/post";  
         $params = array(
@@ -50,18 +73,30 @@ class Letters extends StannpPhp
         return $this->post($path, $params);
     }
 
+    /**
+     * Post a single letter that already has an address on the PDF file.
+     * 
+     * @param string $csv        CSV file containing recipient addresses
+     * @param string $background A letter heading design or a file for the background of every page
+     * @param int    $template   An ID of a template already set up on the platform.
+     * @param string $pages      String containing basic HTML. Pages separated by sending an array 
+     * @param string $file       PDF / DOC file directly. This value can be either a binary file or a URL to a file
+     * @param bool   $test       Produce a sample PDF and not dispatch the letter
+     * 
+     * @return JSON An encoded JSON object
+     */
     public function createBatch(
-        $test,
         $csv,
         $background,
         $template,
         $pages,
-        $file
+        $file,
+        $test = true
     ) {
         $path = "/letters/batch";  
         $params = array(
             "test"       => $test,
-            "csv"  => $csv,
+            "csv"        => $csv,
             "background" => $background,
             "template"   => $template,
             "pages"      => $pages,
